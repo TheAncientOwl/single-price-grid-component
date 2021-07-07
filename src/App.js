79ae74e3284dd7eb8154eebf2a472fc68096392c
TheAncientOwl <dele.alex.o.o@gmail.com>
2021-07-07 13:52:22 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        textAlign: 'center',
+        marginTop: '20x',
+        fontSize: '25px',
+      }}>
+      ~ Hello world! ~
+    </div>
+  );
+}
